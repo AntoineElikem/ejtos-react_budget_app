cdf9225aef0813cbd1e7962ced1dc7b9d25f352f
antoineelikem <antoinedzisam@yahoo.com>
2023-05-18 10:56:59 -0400

    first-commit

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,1 +1,18 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Remaining = () =>{
+    const { expenses, budget } = useContext(AppContext);
+    const totalExpense = expenses.reduce(
+        (total,item) => {
+            return (total = total + item.cost);
+        },0
+    );
+    const alertType = (budget < totalExpense) ? "alert-danger" : "alert-success";
+    return (
+        <div className={`alert ${alertType}`}>
+            <span>Remaining: £{ budget - totalExpense} </span>
+        </div>
+    );
+};
+export default Remaining;

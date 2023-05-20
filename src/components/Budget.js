c1bc4bac0fa6138cd56ca600d463c658c72b08b3
antoineelikem <antoinedzisam@yahoo.com>
2023-05-20 15:40:07 -0400

    budget_not_exceeding

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,37 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+import {useRef, useEffect} from 'react';
  
 const Budget = () => {
     const { budget } = useContext(AppContext);
+    const spending = budget;
+    const inputRef = useRef(null);
+    //document.getElementById("budget_input").value = budget;
+
+    useEffect(() => {
+        if (inputRef.current) {
+          inputRef.current.value = spending;
+        }
+      }, []);
+
+    const handleBudjetChange = (event) => {
+        alert(`spending is ${spending}`);
+        let newValue = event.target.value;
+        
+        if (newValue < spending){
+            alert(`You cannot reduce the budget value lower than the spending`);
+        }
+       
+    }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £</span>
+            <input 
+            ref={ inputRef }
+            type="number" 
+            min="10"
+            step="10"
+            onChange={handleBudjetChange} />
         </div>
     );
 };

3cd0145625cda4028a72875f2e2e819538e522fe
antoineelikem <antoinedzisam@yahoo.com>
2023-06-06 13:37:43 -0400

    currency_change

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -2,6 +2,13 @@ import React, { useState } from 'react';
 
 const Currency = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedCurrency, setSelectedCurrency] = useState('Currency (£ Pound)');
+  
+  const handleSelect = (currency) => {
+    let prefix = "Currency"
+    setSelectedCurrency(prefix + ' (' + currency + ')');
+    setIsOpen(false);
+  };
 
   return (
     <div className={isOpen ? "dropdown show" : "dropdown"}>
@@ -12,16 +19,17 @@ const Currency = () => {
         onClick={() => setIsOpen(!isOpen)}
         style={{ backgroundColor: '#93E49A', border: 'none'}}
       >
-        Dropdown button
+       {selectedCurrency}
       </button>
       <div className={isOpen ? "dropdown-menu show" : "dropdown-menu"} 
         aria-labelledby="dropdownMenuButton"
         style={{border: '2px solid black', marginTop: '5px', backgroundColor: '#93E49A'}}
       >
-        <a className="dropdown-item" href="#">£ Pound</a>
-        <a className="dropdown-item" href="#">€ Euro</a>
-        <a className="dropdown-item" href="#">₹ Rupee</a>
-        <a className="dropdown-item" href="#">$ Dollar</a>
+        <a className="dropdown-item" onClick={() => handleSelect('$ Dollar')} href="#">$ Dollar</a>
+        <a className="dropdown-item" onClick={() => handleSelect('£ Pound')} href="#">£ Pound</a>
+        <a className="dropdown-item" onClick={() => handleSelect('€ Euro')} href="#">€ Euro</a>
+        <a className="dropdown-item" onClick={() => handleSelect('£ Rupee')} href="#">₹ Rupee</a>
+        
       </div>
     </div>
   );

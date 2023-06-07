2ab737941a6ccb147dd4be6e2aff807a0646c87d
antoineelikem <antoinedzisam@yahoo.com>
2023-06-07 09:41:35 -0400

    currency change 03

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
+import { FaPlus, FaMinus } from 'react-icons/fa';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
@@ -24,12 +25,55 @@ const ExpenseItem = (props) => {
         });
 
     }
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: -10,
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+    }
 
     return (
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td> <button 
+            onClick={event=> increaseAllocation(props.name)} 
+            style={{
+                backgroundColor: 'green', 
+                border: 'none',
+                borderRadius: '50%',
+                width: '30px',
+                height: '30px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                color: 'white'
+              }}>
+            <FaPlus style={{color: 'white'}} />
+          </button></td>
+          <td>
+                <button 
+                    onClick={event=> decreaseAllocation(props.name)} 
+                    style={{
+                        backgroundColor: 'red', 
+                        border: 'none',
+                        borderRadius: '50%',
+                        width: '30px',
+                        height: '30px',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        color: 'white'
+                    }}
+                >
+                    <FaMinus />
+                </button>
+            </td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
